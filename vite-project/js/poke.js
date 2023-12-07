586156622b90b3c1839a2c385cd0872c04b0d442
hellov1ncent <113193203+hellov1ncent@users.noreply.github.com>
2023-12-07 09:42:00 -0500

    asdas

--- vite-project/js/poke.js
+++ vite-project/js/poke.js
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-
-
-
-
-
-const URL = 'https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0';
-
-try {
-    //requesting a response REST API's
-    const response = await fetch(URL);
-    if(response.status != 200){
-        throw new Error(response.statusText);
-    }
-    //convert response to JSON
-    const data = await response.json();
-    console.log(data.content);
-} catch (error) {
-    console.log(error, "sorry");
-}
-getData(URL);
-=======
 /* const URLs = `https://api.disneyapi.dev/character`;
 async function getData(URLs) {
     const response = await fetch(URLs);
@@ -27,9 +5,7 @@ async function getData(URLs) {
 }
 getData(URLs);
  */
-
 const URL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
-
 async function getData(url) {
     try {
         const response = await fetch(url);
@@ -39,9 +15,7 @@ async function getData(url) {
         console.error("Error", error);
     }
 }
-
 getData(URL)
     .then(data => {
         console.log(data);
     });
->>>>>>> df11468a892ec9a8698131005f2eef9ff107b5b2
