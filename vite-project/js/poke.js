ca6c74e9d14548e2825ba56a74714ddf67af1a92
hellov1ncent <113193203+hellov1ncent@users.noreply.github.com>
2023-12-06 17:56:01 -0700

    sd

---
+++ vite-project/js/poke.js
@@ -0,0 +1,24 @@
+/* const URLs = `https://api.disneyapi.dev/character`;
+async function getData(URLs) {
+    const response = await fetch(URLs);
+    const data = await response.json();
+}
+getData(URLs);
+ */
+
+const URL = "https://api.disneyapi.dev/character";
+
+async function getData(url) {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data; 
+    } catch (error) {
+        console.error("Error", error);
+    }
+}
+
+getData(URL)
+    .then(data => {
+        console.log(data);
+    })
