c1dd16d4b62768ea24dfc53375b588b70bb96706
hellov1ncent <113193203+hellov1ncent@users.noreply.github.com>
2023-12-13 10:13:54 -0500

    x

--- vite-project/js/poke.js
+++ vite-project/js/poke.js
@@ -18,5 +18,7 @@ async function getData(url) {
 getData(URL)
     .then(data => {
         console.log(data)
-/*         data.results.forEach((name)=>console.log(name)) */
+        data.results.forEach((name)=>console.log(name))
     });
+
+
