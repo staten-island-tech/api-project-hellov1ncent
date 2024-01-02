2843e71ebd19038ce8bfc25142b4375e995de5c5
hellov1ncent <113193203+hellov1ncent@users.noreply.github.com>
2024-01-01 18:18:39 -0700

    sds

---
+++ vite-project/poke.js
@@ -0,0 +1,63 @@
+const DOMSelectors = {
+    column: document.getElementById("pokemon-container"),
+    filterButton: document.getElementById("filterButton"),
+};
+
+let pokemonArray = [];
+
+async function fetchData() {
+    try {
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100");
+        const data = await response.json();
+        pokemonArray = data.results;
+
+        insertCards(pokemonArray);
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+}
+
+function insertCards(arr) {
+    DOMSelectors.column.innerHTML = ''; 
+    arr.forEach(async (pokemon) => {
+        try {
+            const response = await fetch(pokemon.url);
+            const data = await response.json();
+
+            DOMSelectors.column.insertAdjacentHTML(
+                "beforeend",
+                `<div class="card">
+                    <h3 class="name">${data.name}</h3>
+                    <img src="${data.sprites.front_default}" class="img">
+                </div>`
+            );
+        } catch (error) {
+            console.error("Error fetching Pokemon details:", error);
+        }
+    });
+}
+function searchPokemon() {
+    try {
+        const searchTerm = searchInput.value.toLowerCase();
+        const matchingPokemon = pokemonArray.filter(pokemon => pokemon.name.includes(searchTerm));
+        insertCards(matchingPokemon);
+    } catch (error) {
+        console.error("Error in searchPokemon:", error);
+    }
+}
+
+function filterButtonClick() {
+    try {
+        searchPokemon();
+    } catch (error) {
+        console.error("Error in filterButtonClick:", error);
+    }
+}
+
+DOMSelectors.filterButton.addEventListener("click", filterButtonClick);
+
+try {
+    fetchData();
+} catch (error) {
+    console.error("Error in fetchData:", error);
+}
